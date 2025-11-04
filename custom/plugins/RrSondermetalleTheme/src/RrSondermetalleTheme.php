e1fe2ea4e867dbc023c2ac3dbf319704ad0bf6e3
Bita Sadeghi <bita.s9170@gmail.com>
2025-11-04 10:38:57 +0100

    update: bump Shopware to 6.7.3.1 and adjust theme files

---
+++ custom/plugins/RrSondermetalleTheme/src/RrSondermetalleTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace RrSondermetalleTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class RrSondermetalleTheme extends Plugin implements ThemeInterface
+{
+}
